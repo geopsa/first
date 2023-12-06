92bb8d3e3312943e1225d1c664f383c93ec6b096
geopsa <smmilek@gmail.com>
2023-12-06 19:31:49 +0100

    add basic layot

--- coolsite/man/urls.py
+++ coolsite/man/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    #path('', index),
     path('sait/', sait),
+    path('main/', main)
 ]
--- coolsite/man/views.py
+++ coolsite/man/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hello, this is animal world')
+#def index(request):
+    #return HttpResponse('Hello, this is animal world')
+    
 
 def sait(request):
     return render(request, template_name='base.html')
+
+def main(request):
+    return render(request, 'man/index.html')
